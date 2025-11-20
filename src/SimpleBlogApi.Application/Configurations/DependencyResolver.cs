c4d3f2acd31a48dc86a2e8717768b33029175351
Lucas Fernandes Cassetari <lucas.fernandes@ferias.co>
2025-11-20 17:02:31 -0300

    chore: Fixing launchSettings.json

--- src/SimpleBlogApi.Application/Configurations/DependencyResolver.cs
+++ src/SimpleBlogApi.Application/Configurations/DependencyResolver.cs
@@ -1,5 +1,38 @@
-﻿namespace SimpleBlogApi.Application.Configurations;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using SimpleBlogApi.Domain.Interfaces.Repositories;
+using SimpleBlogApi.Infrastructure.Contexts;
+using SimpleBlogApi.Infrastructure.Repositories;
+using System.Reflection;
+
+namespace SimpleBlogApi.Application.Configurations;
 
 public static class DependencyResolver
 {
+    public static IServiceCollection ResolveDependencies(
+        this IServiceCollection services)
+    {
+        services.ResolveRepositories();
+        services.ResolveMediatR();
+
+        return services;
+    }
+
+    private static void ResolveRepositories(this IServiceCollection services)
+    {
+        var connectionString = Environment.GetEnvironmentVariable("SQLSERVER_CONNECTION_STRING");
+
+        services.AddDbContext<BlogDbContext>(_ =>
+            _.UseSqlServer(
+                connectionString, 
+                _ => _.CommandTimeout(180))
+            .EnableSensitiveDataLogging(true));
+
+        services.AddScoped<IPostRepository, PostRepository>();
+        services.AddScoped<ICommentRepository, CommentRepository>();
+    }
+
+    private static void ResolveMediatR(this IServiceCollection services)
+     => services.AddMediatR(cfg => 
+            cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()));
 }
