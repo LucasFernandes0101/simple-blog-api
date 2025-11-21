3298556f8e4fd08fc62b3de50141f3dcd268bd66
Lucas Fernandes Cassetari <lucas.fernandes@ferias.co>
2025-11-20 21:27:49 -0300

    chore: Adding docker compose and automating migrations

--- src/SimpleBlogApi/Program.cs
+++ src/SimpleBlogApi/Program.cs
@@ -3,6 +3,8 @@ using SimpleBlogApi.v1.Configurations;
 using SimpleBlogApi.v1.Middlewares;
 using SimpleBlogApi.Application.Configurations;
 using System.Text.Json.Serialization;
+using SimpleBlogApi.Infrastructure.Contexts;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -25,6 +27,21 @@ builder.Services.AddOpenApi();
 builder.Services.ResolveDependencies();
 
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    try
+    {
+        var db = services.GetRequiredService<BlogDbContext>();
+        db.Database.Migrate();
+    }
+    catch (Exception ex)
+    {
+        Log.Error(ex, "An error occurred while migrating the database.");
+        throw;
+    }
+}
 
 if (app.Environment.IsDevelopment())
 {
